e5bab57a7315dc6c3a1436eaf53418f06a47236e
FadyHabibKelliny <70114794+FadyHabibKelliny@users.noreply.github.com>
2021-06-21 00:17:43 +0200

    Add files via upload

---
+++ File and Folders Bulk Renameing.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 18:28:41 2020
+
+@author: Fady_
+"""   
+
+import os
+path = 'your path here'
+files = os.listdir(path)
+
+
+for index, file in enumerate(files):
+    #for folders uncommint the next line
+    #os.rename(os.path.join(path, file), os.path.join(path, ''.join(['book mockup order ' + str( 82 + index)])))
+    #for images uncommint the next line
+    #os.rename(os.path.join(path, file), os.path.join(path, ''.join([ str( 1 + index), '.png'])))
