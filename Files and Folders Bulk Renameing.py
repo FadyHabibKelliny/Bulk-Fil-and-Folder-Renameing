36e98e2eab09c1a15ec381c1ef0a50b24d934128
FadyHabibKelliny <70114794+FadyHabibKelliny@users.noreply.github.com>
2021-06-21 00:28:59 +0200

    Update Files and Folders Bulk Renameing.py

--- Files and Folders Bulk Renameing.py
+++ Files and Folders Bulk Renameing.py
@@ -13,5 +13,5 @@ files = os.listdir(path)
 for index, file in enumerate(files):
     #for folders uncommint the next line
     #os.rename(os.path.join(path, file), os.path.join(path, ''.join(['book mockup order ' + str( 82 + index)])))
-    #for images uncommint the next line
+    #for file uncommint the next line and change the file extention.
     #os.rename(os.path.join(path, file), os.path.join(path, ''.join([ str( 1 + index), '.png'])))
